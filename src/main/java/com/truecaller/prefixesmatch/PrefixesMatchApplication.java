d5d131ecba505170eca139daa00b976da5296c1b
R.Maurya <rishikesh.maurya@myntra.com>
2022-08-28 11:37:18 +0530

    initial commit

---
+++ src/main/java/com/truecaller/prefixesmatch/PrefixesMatchApplication.java
@@ -0,0 +1,13 @@
+package com.truecaller.prefixesmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefixesMatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrefixesMatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/truecaller/prefixesmatch/PrefixesMatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truecaller.prefixesmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefixesMatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
